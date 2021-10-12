1b0ca333d514413c694318dc22129d9f07c7cf92
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-12 14:27:52 -0600

    Base app working using axios to get coin data using promises, useState, useEffect. Need to make the data look nice now.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Coin from './Coin';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1>Search a currency</h1>
+        <form>
+          <input
+            onChange={handleChange}
+            type='text'
+            className='coin-input'
+            placeholder='search'
+          ></input>
+        </form>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.price}
+              image={coin.image}
+              symbol={coin.symbol}
+              volume={coin.market_cap}
+            />
+          );
+        })}
+      </div>
+      <submit>Submit</submit>
+    </div>
+  );
+}
+
+export default App;
